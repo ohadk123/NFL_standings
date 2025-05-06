2bf8d1b58c66a50fca535011f2a8747d9a2b4eb6
ohadk123 <ohad43@gmail.com>
2025-05-06 19:58:26 +0300

    added "favicon" changed route

--- src/main/java/com/example/demo/StandingController.java
+++ src/main/java/com/example/demo/StandingController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,7 +16,7 @@ public class StandingController {
         this.repo = repo;
     }
 
-    @GetMapping("/standings")
+    @GetMapping("/")
     public String showStandings(Model model) {
         List<StandingClass> standings = repo.getAllFromView();
 
